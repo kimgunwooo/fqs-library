83bfc77c378c4646c82b005889a57b64f474fb7d
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-10-17 18:53:08 +0900

    setting : library 활용방법 공유를 위한 example module setting

---
+++ example/src/test/java/com/f4/fqs_library/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f4.fqs_library.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
