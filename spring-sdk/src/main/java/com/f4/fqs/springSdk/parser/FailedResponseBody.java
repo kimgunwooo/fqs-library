e9be3b02543f76f0805f70ef227352f6077d3f83
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-10-19 21:08:50 +0900

    feat : FQS 서버에서 사용하는 공통 응답 코드를 추가

---
+++ spring-sdk/src/main/java/com/f4/fqs/springSdk/parser/FailedResponseBody.java
@@ -0,0 +1,21 @@
+package com.f4.fqs.springSdk.parser;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+@JsonTypeName("false")
+public final class FailedResponseBody extends ResponseBody<Void> {
+
+    private String msg;
+
+    protected FailedResponseBody() {
+    }
+
+    public FailedResponseBody(String code, String msg) {
+        this.setCode(code);
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
---
+++ spring-sdk/src/main/java/com/f4/fqs/springSdk/parser/SuccessResponseBody.java
@@ -0,0 +1,20 @@
+package com.f4.fqs.springSdk.parser;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+@JsonTypeName("true")
+public final class SuccessResponseBody<T> extends ResponseBody<T> {
+    private final T data;
+
+    public SuccessResponseBody() {
+        data = null;
+    }
+
+    public SuccessResponseBody(T result) {
+        this.data = result;
+    }
+
+    public T getData() {
+        return data;
+    }
+}
