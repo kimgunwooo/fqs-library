6d2d1acb2ea752e16d898034795bd59d31d04d07
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-10-19 21:12:14 +0900

    chore : interface를 통해 내부 로직을 숨기고, 해당 메서드의 설명 추가

--- spring-sdk/src/main/java/com/f4/fqs/springSdk/constants/FQSSdk.java
+++
@@ -1,8 +0,0 @@
-package com.f4.fqs.springSdk.constants;
-
-public interface FQSSdk {
-
-  String SECRET_KEY = "X-Secret-Key";
-  String FQS_SERVER_URL = "http://localhost:19095";
-  String VALIDATE_ENDPOINT = "/api/queue/validate";
-}
